9850f14bca8e849220753a84f8b831e43598f629
spaceguy01 <joonyi@gmail.com>
2018-10-08 23:45:55 -0700

    deleted unwanted folder

--- Tutor/tutorsite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TutorsiteConfig(AppConfig):
-    name = 'tutorsite'
--- Tutor/tutorsitekr/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TutorsitekrConfig(AppConfig):
-    name = 'tutorsitekr'
