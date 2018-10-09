1fd00c9d0269a79d9253bd699f6f0e1bb74893aa
spaceguy01 <joonyi@gmail.com>
2018-10-08 21:12:38 -0700

    changes to mywebsite

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
