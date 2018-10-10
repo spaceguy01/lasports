42383385c5a848d8183c4565fd48b82f616101d6
spaceguy01 <joonyi@gmail.com>
2018-10-10 12:26:36 -0700

    changed whitenoise

--- lasports/lasports/settings.py
+++ lasports/lasports/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'lasports.urls'
