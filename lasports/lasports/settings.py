8e7f099b199a4c899534d1129e312c3a8c0c506f
spaceguy01 <joonyi@gmail.com>
2018-10-10 09:42:53 -0700

    template dir change

--- lasports/lasports/settings.py
+++ lasports/lasports/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'lasports.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [TEMPLATES_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
