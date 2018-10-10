7010b8ab1849a62dc2d61bafa06278f9d57a70f2
spaceguy01 <joonyi@gmail.com>
2018-10-10 14:04:32 -0700

    staticfiles

--- lasports/lasports/settings.py
+++ lasports/lasports/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
