1712f33f77b98861733789da47e280e03c4f4605
spaceguy01 <joonyi@gmail.com>
2018-10-10 20:48:42 -0700

    changed setting took out white noise

--- lasports/lasports/settings.py
+++ lasports/lasports/settings.py
@@ -133,7 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
