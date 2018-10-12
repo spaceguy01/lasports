8bad78fc8df328a19540f793a4b43ef797e10e21
spaceguy01 <joonyi@gmail.com>
2018-10-11 20:27:06 -0700

    changed url shop

--- lasports/lasports/urls.py
+++ lasports/lasports/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('', views.home, name="home"),
+    path('shop/', include('shop.urls')),
     path('search/', include('search.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
